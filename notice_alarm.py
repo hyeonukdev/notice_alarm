180f4a0ebb5bf23e5ae6f7c1afb173f07b040ee2
hyeonukdev <lego7407@gmail.com>
2019-07-26 18:03:56 +0900

    ver1.0.3

--- notice_alarm.py
+++ notice_alarm.py
@@ -1,22 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import os
 import telegram
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    notice()
-    print('This job is run every 1 minutes.')
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     notice()
+#     print('This job is run every 1 minutes.')
 
 def notice():
     bot = telegram.Bot(token='849308859:AAF7aPnWtSjI8evIQIU4od1tA6vZgpEwjzg')
     chat_id = bot.getUpdates()[-1].message.chat.id
-
-    # 파일의 위치
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     req = requests.get('https://www.konyang.ac.kr/cop/bbs/BBSMSTR_000000000582/selectBoardList.do')
     req.encoding = 'utf-8' # Clien에서 encoding 정보를 보내주지 않아 encoding옵션을 추가해줘야합니다.
@@ -31,7 +27,7 @@ def notice():
     print(latestlink)
     url = 'https://www.konyang.ac.kr' + latestlink
 
-    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
+    with open('./latest.txt', 'r+') as f_read:
         before = f_read.readline()
         if before != latest:
             bot.sendMessage(chat_id=chat_id, text='새 글이 올라왔어요!')
@@ -41,8 +37,9 @@ def notice():
             bot.sendMessage(chat_id=chat_id, text='새 글이 없어요 ㅠㅠ')   
         f_read.close()
 
-    with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
+    with open('./latest.txt', 'w+') as f_write:
         f_write.write(latest)
         f_write.close()
 
-sched.start()
+# sched.start()
+notice()
