1d42eadfb32b1999678eecc6e0718b3e56a6bd53
hyeonukdev <lego7407@gmail.com>
2019-07-26 22:02:21 +0900

    ver2.1.3

--- new_notice_alaram_bot.py
+++ new_notice_alaram_bot.py
@@ -21,14 +21,14 @@ def notice():
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
     posts = soup.select('table > tbody > tr:nth-child(1) > td.left > div > span.link > a > b')
-    links = soup.select('table > tbody > tr:nth-child(1) > td.left > div > span.link > a ')
+    # links = soup.select('table > tbody > tr:nth-child(1) > td.left > div > span.link > a ')
     latest=posts[0].text
-    latestlink=links[0]['href']
+    # latestlink=links[0]['href']
     print(latest)
-    print(latestlink)
-    url = 'https://www.konyang.ac.kr' + latestlink
+    # print(latestlink)
+    url = 'https://www.konyang.ac.kr/cop/bbs/BBSMSTR_000000000582/selectBoardList.do'
 
-    with open('./notice_latest.txt', 'r+', encoding='utf-8') as f_read:
+    with open('./latest.txt', 'r+', encoding='utf-8') as f_read:
         before = f_read.readline()
         if before != latest:
             bot.sendMessage(chat_id=chat_id, text='새 글이 올라왔어요!')
@@ -38,7 +38,7 @@ def notice():
             bot.sendMessage(chat_id=chat_id, text='새 글이 없어요 ㅠㅠ')   
         f_read.close()
 
-    with open('./notice_latest.txt', 'w+', encoding='utf-8') as f_write:
+    with open('./latest.txt', 'w+', encoding='utf-8') as f_write:
         f_write.write(latest)
         f_write.close()
 
