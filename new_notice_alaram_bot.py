24a1416f89545c892060c6077822c77024d80120
hyeonukdev <lego7407@gmail.com>
2019-07-30 17:18:31 +0900

    1520

--- new_notice_alaram_bot.py
+++ new_notice_alaram_bot.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minutes=20)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=20)
 def timed_job():
     notice()
 
